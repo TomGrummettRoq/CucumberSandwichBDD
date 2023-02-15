882ea0d074a2d07f661000ec85c72264628e3d57
Tom Grummmett <tom.grummett@roq.co.uk>
2023-02-15 15:53:47 +0000

    Initial Cucumber Scenarios and Steps

--- features/fibonacci.feature
+++ features/fibonacci.feature
@@ -13,3 +13,30 @@ Feature: Fibonacci number sequences
     When the sequence is skipped 3 times
     Then the state should be "The current number is 8"
     And the next number should be 13
+
+  Scenario: Sequence is initialized to a non-fibonacci number
+    Given a fibonacci sequence initialized to 7
+    Then it should throw an exception
+
+  Scenario: Sequence is initialized to a decimal number
+    Given a fibonacci sequence that is initialized to 6.5
+    Then it should throw an exception
+    
+  Scenario: Sequence is initialized and skipped by '1' multiple times
+    Given a fibonacci sequence initialized to 3
+    When the next number is called
+    And the next number is called
+    And the next number is called
+    Then the state should be "the current number is 21"
+
+  Scenario: Sequence is initialized to a negative number
+    Given a fibonacci sequence initialized to -2
+    Then it should throw an exception
+
+  Scenario: Sequence re-initializes to a determined number after skipping
+    Given a fibonacci sequence initialized to 5
+    When the sequence is skipped 3 times
+    And the fibonacci sequence is then initialized to 3
+    Then the state should be "the current number is 3"
+
+  
--- features/support/steps.js
+++ features/support/steps.js
@@ -1,13 +1,13 @@
 /*
- ******************************************************************************
- * Cucumber/Gherkin
- * ============================================================================
- * 
- * NOTES:
- * - Arrow functions not supported by cucumber when accessing the 'world' class
- *   SEE: github.com/cucumber/cucumber-js/blob/main/docs/support_files/world.md
- * 
- ******************************************************************************
+******************************************************************************
+* Cucumber/Gherkin
+* ============================================================================
+* 
+* NOTES:
+* - Arrow functions not supported by cucumber when accessing the 'world' class
+*   SEE: github.com/cucumber/cucumber-js/blob/main/docs/support_files/world.md
+* 
+******************************************************************************
 */
 
 const assert = require('assert');
@@ -18,6 +18,22 @@ const {Given, When, Then} = require('@cucumber/cucumber');
 // ----------------------------------------------------------------------------
 
 Given("a fibonacci sequence initialized to {int}", function(value) {
+        try {
+            this.fibonacci.init(value);
+        } catch {
+            this.itThrew();
+        }
+    });
+
+Given("a fibonacci sequence that is initialized to {float}", function(float) {
+        try {
+            this.fibonacci.init(float);
+        } catch {
+            this.itThrew();
+        }
+    });
+
+When("the fibonacci sequence is then initialized to {int}", function(value) {
     this.fibonacci.init(value);
 });
 
@@ -29,6 +45,10 @@ When("the sequence is skipped {int} time(s)", function(value) {
     this.fibonacci.skip(value);
 });
 
+When("the next number is called", function(value) {
+    this.fibonacci.next(value);
+});
+
 Then("the next number should be {int}", function(value) {
     assert(this.fibonacci.next() == value);
 });
@@ -36,3 +56,9 @@ Then("the next number should be {int}", function(value) {
 Then("the state should be {string}", function(value) {
     assert(this.fibonacci.state() == value);
 });
+
+Then("it should throw an exception", function() {
+    assert(this.hasThrown());
+});
+
+
